fec9776f88e29f08ece542c771346e046d36c4cd
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-26 15:36:07 +0200

    Update ExmpController.php

--- app/Http/Controllers/ExmpController.php
+++ app/Http/Controllers/ExmpController.php
@@ -10,11 +10,15 @@ class ExmpController extends Controller
         'keyFilePath' => base_path('gc.json'),
         'projectId' => 'sample-207012'
     ]);
-
+    
     $languege = $cloud->language->();
-
+        //this is the text to analise
     $text = 'I hat this - why did they not make provisions?';
 
+    //this is to ditect the sentiment of the text
     $annotation = $languege->analyzeStatment($text);
-    $statment = $annotation->sentiment();
+    $sentiment = $annotation->sentiment();
+
+    echo 'Sentiment Score: ' .$s
+
 }
